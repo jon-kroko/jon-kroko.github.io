76413c9b7301b1e0b96500111ef1b21b58dcd8ff
jon-kroko <37243299+jon-kroko@users.noreply.github.com>
2020-12-07 16:46:37 +0100

    Prettify map view

--- leaflet_map.js
+++ leaflet_map.js
@@ -1,10 +1,11 @@
 // Initialize Leaflet map in HTML element "map" (https://leafletjs.com/)
-var map = L.map('map').setView([20, -20], 2);
+var map = L.map('map').setView([0, 0], 2);
 
 // Add satellite images as map layer
 var satelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    minZoom: 1,
     id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
